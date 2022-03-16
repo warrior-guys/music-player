203bc045691e3fd9df79b1e74b6c05b9ee494ce4
Aayush Shukla <oreus.aks@gmail.com>
2022-03-16 08:55:25 +0530

    Custom songs played

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from tkinter import *
 from pygame import mixer
+from PIL import Image, ImageTk
+import functools
 
 root = Tk()
 root.geometry('1280x720')
@@ -25,26 +27,57 @@ listframel.grid_columnconfigure((0,), weight=1)
 listframer.grid_rowconfigure((0,1,2,3,4), weight=1)
 listframer.grid_columnconfigure((1,), weight=1)
 
+played_song = 0
+song_index = 0
+changed_song = 0
 
 listbox = Listbox(listframel)
 listbox.grid(row=0, column=0, rowspan=5, sticky=NSEW)
+song_name = ""
+
+def fileSelection(self):
+    global song_index, changed_song
+    s = song_index
+    selection = listbox.curselection()
+    song_index = functools.reduce(lambda sub, ele: sub * 10 + ele, selection)
+    if s != song_index:
+        changed_song = 1
+    
+listbox.bind("<<ListboxSelect>>", fileSelection)
 
 for song in song_list:
     if song.endswith(".mp3"):
         lst.append(str(song))
-        path="E:\TEAM YTB\Songs\\"+song
-        mixer.music.load(str(path))
 
 def play_song():
-    mixer.music.play()
+    global played_song, changed_song, song_index
+    print(changed_song)
+    print(song_index)
+
+    if played_song == 0:
+        path = "E:\TEAM YTB\Songs\\" + lst[song_index]
+        mixer.music.load(str(path))
+        mixer.music.play()
+        played_song = 1
+    elif played_song == 1:
+        if changed_song == 0:
+            mixer.music.unpause()
+        elif changed_song == 1:
+            path = "E:\TEAM YTB\Songs\\" + lst[song_index]
+            mixer.music.load(str(path))
+            mixer.music.play()
+            changed_song = 0
 
 def pause_song():
     mixer.music.pause()
 
-play = Button(root, command=play_song, text="Play")
+play_img = ImageTk.PhotoImage(Image.open('play.png').resize((100,100)))
+pause_img = ImageTk.PhotoImage(Image.open('pause.png').resize((100,100)))
+
+play = Button(root, image=play_img, command=play_song)
 play.grid(row=5, column=0)
 
-pause = Button(root, command=pause_song, text="Pause")
+pause = Button(root, image=pause_img, command=pause_song)
 pause.grid(row=5, column=1)
 
 for i in range(len(lst)):
