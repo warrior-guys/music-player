91e06058cd1b6372f8bd49d589d2d7c09c421dc9
Shivam Kumar <80425396+Shiva-slbs@users.noreply.github.com>
2022-03-17 10:35:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from tkinter import *
-from pygame import mixer
+from tkinter import font as tkFont
+import pygame
 from PIL import Image, ImageTk
 import functools
 
@@ -11,15 +12,21 @@ root.grid_rowconfigure((0,1,2,3,4,5), weight=1)
 root.grid_columnconfigure((0,), weight=1)
 root.grid_columnconfigure((1,), weight=5)
 root.resizable(False, False)
-mixer.init()
+root.configure(bg="black")
+root.option_add('*Dialog.msg.font', 'Segoe 11')
+pygame.init()
+pygame.mixer.init()
 
 song_list = os.listdir("E:\TEAM YTB\Songs\\")
 lst = list()
+conforta = tkFont.Font(family="conforta", size=11)
 
-listframel = Frame(root, highlightbackground="black", highlightthickness=5)
+listframel = Frame(root)
 listframel.grid(row=0, column=0, sticky=NSEW, rowspan=5)
-listframer = Frame(root, highlightbackground="black", highlightthickness=5)
+listframel.configure(bg="white")
+listframer = Frame(root)
 listframer.grid(row=0, column=1, sticky=NSEW, rowspan=5)
+listframer.configure(bg="white")
 
 listframel.grid_rowconfigure((0,1,2,3,4), weight=1)
 listframel.grid_columnconfigure((0,), weight=1)
@@ -27,13 +34,25 @@ listframel.grid_columnconfigure((0,), weight=1)
 listframer.grid_rowconfigure((0,1,2,3,4), weight=1)
 listframer.grid_columnconfigure((1,), weight=1)
 
+play_img = ImageTk.PhotoImage(Image.open('E:\Projects\Media Player\play.png').resize((50,50)))
+pause_img = ImageTk.PhotoImage(Image.open('E:\Projects\Media Player\pause.png').resize((50,50)))
+
 played_song = 0
 song_index = 0
 changed_song = 0
 
-listbox = Listbox(listframel)
-listbox.grid(row=0, column=0, rowspan=5, sticky=NSEW)
+scrollbar = Scrollbar(listframel)
+scrollbar.pack(side=RIGHT, fill=Y)
+
+scrollbar2 = Scrollbar(listframel, orient='horizontal')
+scrollbar2.pack(side=BOTTOM, fill=X)
+
+listbox = Listbox(listframel, xscrollcommand=scrollbar2.set, yscrollcommand=scrollbar.set, font=conforta, borderwidth=0)
+listbox.pack(fill=BOTH, expand=1)
 song_name = ""
+
+def browse():
+    pass
 
 def fileSelection(self):
     global song_index, changed_song
@@ -51,38 +70,64 @@ for song in song_list:
 
 def play_song():
     global played_song, changed_song, song_index
-    print(changed_song)
-    print(song_index)
 
-    if played_song == 0:
-        path = "E:\TEAM YTB\Songs\\" + lst[song_index]
-        mixer.music.load(str(path))
-        mixer.music.play()
-        played_song = 1
-    elif played_song == 1:
-        if changed_song == 0:
-            mixer.music.unpause()
-        elif changed_song == 1:
+    if pygame.mixer.music.get_busy():
+        pygame.mixer.music.pause()
+        play.config(image=play_img)
+    else:
+        if played_song == 0:
             path = "E:\TEAM YTB\Songs\\" + lst[song_index]
-            mixer.music.load(str(path))
-            mixer.music.play()
-            changed_song = 0
+            pygame.mixer.music.load(str(path))
+            pygame.mixer.music.play()  
+            played_song = 1
+        elif played_song == 1:
+            if changed_song == 0:
+                pygame.mixer.music.unpause()
+            elif changed_song == 1:
+                path = "E:\TEAM YTB\Songs\\" + lst[song_index]
+                pygame.mixer.music.load(str(path))
+                pygame.mixer.music.play()
+                changed_song = 0
+        play.config(image=pause_img)
 
 def pause_song():
-    mixer.music.pause()
+    pygame.mixer.music.pause()
 
-play_img = ImageTk.PhotoImage(Image.open('play.png').resize((100,100)))
-pause_img = ImageTk.PhotoImage(Image.open('pause.png').resize((100,100)))
+    if pygame.mixer.music.get_busy():
+        play.config(image=play_img)
+    else:
+        play.config(image=pause_img)
 
-play = Button(root, image=play_img, command=play_song)
+def change_song():
+    global changed_song, song_index
+
+    for event in pygame.event.get():
+        if event.type == MUSIC_END:
+            changed_song = 1
+            song_index += 1
+            play.config(image=play_img)
+            play_song()
+    
+    root.after(100, change_song)
+
+MUSIC_END = pygame.USEREVENT+1
+pygame.mixer.music.set_endevent(MUSIC_END)
+
+play = Button(root, image=play_img, command=play_song, borderwidth=0, bg="black")
 play.grid(row=5, column=0)
-
-pause = Button(root, image=pause_img, command=pause_song)
-pause.grid(row=5, column=1)
 
 for i in range(len(lst)):
     lg = lst[i]
     lgl = len(lg)
     listbox.insert(i+1, lg[:lgl - 4])
 
+menu = Menu(root)
+filemenu = Menu(menu, tearoff=0)
+filemenu.add_command(label="Browse", command=browse)
+menu.add_cascade(label="File", menu=filemenu)
+
+scrollbar.config(command=listbox.yview)
+scrollbar2.config(command=listbox.xview)
+root.config(menu=menu)
+change_song()
 root.mainloop()
